b1cf73bd7970f72e1e6b930f41b767b881794990
JeftePonce <jefte.ponce.h@mail.pucv.cl>
2021-06-23 13:48:13 -0400

    avance del menu
    se avanza en el esqueleto del menu como tal, sin incluir grandes avances ni funciones aun

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 
 int main(){
 
+    int op;
+    int opInicioSesion;
+
     while(1){//Muestra el menu
 
-        int op;
         printf("QUON MUSIC\n");
         printf("1. Registrarse \n");
         printf("2. Iniciar sesion \n");
@@ -22,25 +24,46 @@ int main(){
 
         case 1:
 
-            printf("Elija un nombre de usuario\n");
-
+            printf("Ingreso a Registrarse\n");
+            printf("Ingrese nombre de usuario\n");
+            //un scanf del nombre de usuario
+            printf("Ingrese contrasena\n");
+            //un scanf de la contrasena
 
             break;
 
         case 2:
 
-            printf("Ingrese Usuario\n");
+
+            while(1){
+
+                printf("Iniciar Sesion\n");
+                printf("Ingrese nombre de usuario\n");
+                //scanf() del nombre de usuario
+                printf("Ingrese contrasena\n");
+                scanf("%i", &opInicioSesion);//estoy escaneando un int aca, hay qye cambiarlo, solo lo hice para avanzar y probar un poco el menu
+
+                if( opInicioSesion == 2 ){/**inicio de sesion valido, entra al menu principal de la aplicacion**/
+
+                    printf("Inicio sesion correctamente\n");
+
+                }else{
+
+                    printf("Datos incorrectos\n");
+
+                }
+
+            }
 
             break;
 
         default:
 
+            printf("Opcion invalida");
+
             break;
 
         }
-
-
-
 
     }
 
