f3963fb16e5caa5ecc82d6f75185fb935887c932
prindoxx <pablortiz132@gmail.com>
2021-06-24 23:54:14 -0400

    TDAs lista y mapas añadidas

---
+++ funciones.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCIONES_H
+#define _FUNCIONES_H
+#include "map.h"
+
+//typedef
+
+const char *get_csv_field (char * tmp, int k);
+
+
+#endif
--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funciones.h"
+#include "list.h"
+#include "map.h"
 
 int main(){
 
