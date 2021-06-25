34b5206fb1c71cdfecba55fbea3d94922454e81d
JeftePonce <jefte.ponce.h@mail.pucv.cl>
2021-06-24 23:07:55 -0400

    avance menu
    se avanza en lo que serian las bases del menu

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ int main(){
 
     int op;
     int opInicioSesion;
+    int opMenu;
 
     while(1){//Muestra el menu
 
-        printf("QUON MUSIC\n");
-        printf("1. Registrarse \n");
-        printf("2. Iniciar sesion \n");
-        printf("0. Salir \n");
+        printf("---------------------QUON MUSIC---------------------\n");
+        printf("-----------------1. Registrarse---------------------\n");
+        printf("----------------2. Iniciar sesion-------------------\n");
+        printf("--------------------0. Salir------------------------\n");
         printf("Escriba el numero de la opcion: ");
         scanf("%i", &op);
+        system("clear");
 
 
         switch (op){
@@ -24,10 +26,10 @@ int main(){
 
         case 1:
 
-            printf("Ingreso a Registrarse\n");
-            printf("Ingrese nombre de usuario\n");
+            printf("--------------------Registrarse---------------------\n");
+            printf("Ingrese nombre de usuario: \n");
             //un scanf del nombre de usuario
-            printf("Ingrese contrasena\n");
+            printf("Ingrese contrasena:\n");
             //un scanf de la contrasena
 
             break;
@@ -37,15 +39,83 @@ int main(){
 
             while(1){
 
-                printf("Iniciar Sesion\n");
-                printf("Ingrese nombre de usuario\n");
+                printf("-------------------Iniciar Sesion-------------------\n");
+                printf("Ingrese nombre de usuario:\n");
                 //scanf() del nombre de usuario
-                printf("Ingrese contrasena\n");
+                printf("Ingrese contrasena:\n");
                 scanf("%i", &opInicioSesion);//estoy escaneando un int aca, hay qye cambiarlo, solo lo hice para avanzar y probar un poco el menu
+                system("clear");
 
                 if( opInicioSesion == 2 ){/**inicio de sesion valido, entra al menu principal de la aplicacion**/
 
                     printf("Inicio sesion correctamente\n");
+
+                    while(1){
+
+                        printf("Menu\n");
+                        printf("1.Explorar Canciones\n");
+                        printf("2.Recomendaciones\n");//submenu
+                        printf("3.Ver mis Canciones\n");//submenu
+                        printf("4.Ver mis Artistas\n");//submenu
+                        printf("5.Ver mis Generos\n");//submenu
+                        printf("6.Cerrar Sesion\n");
+                        printf("7.Atras\n");
+                        printf("0.Salir\n");
+                        printf("Escriba el numero de la opcion: ");
+                        scanf("%i", &opMenu);
+                        system("clear");
+
+                        switch(opMenu){
+
+                        case 0:
+
+                            return 0;
+
+                        case 1:
+
+                            printf("EXPLORAR CANCIONES\n");
+                            break;
+
+                        case 2:
+
+                            printf("RECOMENDACIONES\n");
+                            break;
+
+                        case 3:
+
+                            printf("CANCIONES\n");
+                            break;
+
+                        case 4:
+
+                            printf("ARTISTAS\n");
+                            break;
+
+                        case 5:
+
+                            printf("GENEROS\n");
+                            break;
+
+                        case 6:
+
+                            printf("CERRAR SESION\n");
+                            break;
+
+                        case 7:
+
+                            break;
+
+                        default:
+
+                            printf("Opcion invalida");
+                            break;
+
+                        }
+
+                        break;
+
+
+                    }
 
                 }else{
 
@@ -60,7 +130,6 @@ int main(){
         default:
 
             printf("Opcion invalida");
-
             break;
 
         }
